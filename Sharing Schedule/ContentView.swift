a8dd8a2a0fceb7dff8c8ee8269217ac6edba1f0d
Adison4X <rui.miyashita@gmail.com>
2024-02-15 22:42:30 +0900

    Initial Commit

---
+++ Sharing Schedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sharing Schedule
+//
+//  Created by Rui Miyashita on 2024/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sharing Schedule/Sharing_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sharing_ScheduleApp.swift
+//  Sharing Schedule
+//
+//  Created by Rui Miyashita on 2024/02/15.
+//
+
+import SwiftUI
+
+@main
+struct Sharing_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
