4ff0faf49ca218c69b4fe149456cdf850dbabee7
Adison4X <rui.miyashita@gmail.com>
2024-02-19 22:33:08 +0900

    Add TabView

---
+++ Sharing Schedule/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Sharing Schedule
+//
+//  Created by Rui Miyashita on 2024/02/19.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
